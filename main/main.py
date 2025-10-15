ed6a37ed1c1106aeb8bdf8caf7469739af9de800
KhoirulHS <wibuadalahrasterkuat@gmail.com>
2025-10-15 18:21:02 +0700

    style: add new description

--- main/main.py
+++ main/main.py
@@ -2,16 +2,17 @@ import tkinter as tk
 import customtkinter as ctk
 from datetime import datetime, timedelta
 
-# Ukuran panel
+# Ukuran panel berdasarkan pixel
 size = "500x300"
 
-# Waktu Pomodoro (menit)
+# Waktu dan kata2 penyemangat Pomodoro (menit)
 pomodoro = [25, 5, 25, 5, 25, 5, 25, 30]
 sesion_name = ["belajar dengan semangat", "istirahat kecil", 
                "belajar giat mencapai cita", "istirahat kecil", 
                "belajar adalah investasi", "istirahat kecil",
                "belajar untuk masa depan", "istirahat besar"]
 
+# Kelas yang menampung panel utama aplikasi
 class App(ctk.CTk):
     def __init__(self, pomodoro_time, fg_color=None, **kwargs):
         super().__init__(fg_color=fg_color, **kwargs)
@@ -41,6 +42,7 @@ class App(ctk.CTk):
         self.start_button.pack(pady=20, padx=20, fill="x")
 
     def start_pomodoro(self):
+        # Cek kondisi counter jika lebih dari panjang array pomodoro maka hentikan countdown
         if self.counter >= len(self.pomodoro):
             self.clock_now.configure(text="Pomodoro Time!")
             self.start_button.configure(state="normal", text="Mulai Lagi", command=self.start_pomodoro)
@@ -49,6 +51,8 @@ class App(ctk.CTk):
             self.progress_bar.set(0)
             return
         
+        # Kondisi ketika counter kurang dari panjang array pomodoro
+        # Lanjutkan untuk update clock
         session_text = f"{self.counter+1} : {sesion_name[self.counter]}"
         self.session.configure(text=f"Sesi {session_text}")
         self.start_button.configure(state="disabled", text = "...")
@@ -57,7 +61,7 @@ class App(ctk.CTk):
         self.update_clock()
 
     def update_clock(self):
-
+        # Update tampilan clock selama sekian menit
         minutes, seconds = divmod(self.countdown, 60)
         self.clock_now.configure(text=f"{minutes:02d}:{seconds:02d}")
 
@@ -65,6 +69,8 @@ class App(ctk.CTk):
         progress = 1 - (self.countdown / self.progress_time)
         self.progress_bar.set(progress)
 
+        # Kondisi pengecekan countdown
+        # Lanjutkan countdown jika detik belum 0
         if self.countdown < 0:
             self.counter += 1
             self.start_pomodoro()
@@ -72,6 +78,7 @@ class App(ctk.CTk):
             self.countdown -= 1
             self.after(1000, self.update_clock)
 
+# Entry Point Program
 if __name__ == "__main__":
     app = App(pomodoro_time=pomodoro)
     app.mainloop()
